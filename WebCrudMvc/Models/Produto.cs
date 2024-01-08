8c99fc7ae441fb20ee4a5a47389e5c55b5c071ba
NVanitas <nicolasdasilvasantos04@gmail.com>
2024-01-08 13:40:12 -0300

    Crud Completo Asp.net

---
+++ WebCrudMvc/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCrudMvc.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public String Nome { get; set; }
+    }
+}
